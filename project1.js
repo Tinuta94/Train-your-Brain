f3a3df2a4e7a8dcd0e368874a9a244aa059c61e7
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-07 19:01:48 -0400

    next

--- project1.js
+++ project1.js
@@ -1,3 +1,21 @@
-document.getElementsByClassName('list-item1').addEventListener('click,', function(event)) {
-    
+//let playerOne;
+//document.getElementById('sub').addEventListener('click', function(event) {
+//let playerName = document.querySelector('#name')
+document.getElementById('g1').addEventListener('click', ()=>gOne())
+const gOne = function() {
+    console.log('changing this')
+let firstBox = document.createElement('div')
+firstBox.innerHTML = 'text'
+document.body.append(firstBox)
+firstBox.setAttribute("id", "question");
+let secondBox = document.createElement('div')
+secondBox.innerHTML = 'text'
+document.body.append(secondBox)
+secondBox.setAttribute("id", "answer") 
+let buttonOne = document.createElement('button')
+buttonOne.innerHTML = 'button1'
+secondBox.appendChild(buttonOne)
 }
+
+
+
