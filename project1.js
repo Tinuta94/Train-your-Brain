6fd87ef68982f4906c6b54fc2f5e3df04a818909
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-07 19:13:15 -0400

    next

--- project1.js
+++ project1.js
@@ -4,10 +4,7 @@
 document.getElementById('g1').addEventListener('click', ()=>gOne())
 const gOne = function() {
     console.log('changing this')
-let firstBox = document.createElement('div')
-firstBox.innerHTML = 'text'
-document.body.append(firstBox)
-firstBox.setAttribute("id", "question");
+    firstBoxCreator("who is the president?")
 let secondBox = document.createElement('div')
 secondBox.innerHTML = 'text'
 document.body.append(secondBox)
@@ -15,6 +12,13 @@ secondBox.setAttribute("id", "answer")
 let buttonOne = document.createElement('button')
 buttonOne.innerHTML = 'button1'
 secondBox.appendChild(buttonOne)
+}
+const firstBoxCreator = function(question, answer1, answer2, answer3){
+
+    let firstBox = document.createElement('div')
+    firstBox.innerHTML = question
+    document.body.append(firstBox)
+    firstBox.setAttribute("id", "question");
 }
 
 
