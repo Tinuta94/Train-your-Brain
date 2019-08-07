7cc232270420b9c06a163589a170aa7c36d39fe3
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-07 19:23:09 -0400

    next

--- project1.js
+++ project1.js
@@ -1,24 +1,27 @@
 //let playerOne;
 //document.getElementById('sub').addEventListener('click', function(event) {
 //let playerName = document.querySelector('#name')
-document.getElementById('g1').addEventListener('click', ()=>gOne())
-const gOne = function() {
+document.getElementById('g1').addEventListener('click', () => gOne())
+const gOne = function () {
     console.log('changing this')
     firstBoxCreator("who is the president?")
-let secondBox = document.createElement('div')
-secondBox.innerHTML = 'text'
-document.body.append(secondBox)
-secondBox.setAttribute("id", "answer") 
-let buttonOne = document.createElement('button')
-buttonOne.innerHTML = 'button1'
-secondBox.appendChild(buttonOne)
+    secondBoxCreator("a,b,c")
 }
-const firstBoxCreator = function(question, answer1, answer2, answer3){
+const firstBoxCreator = function (question, a, b, c) {
 
     let firstBox = document.createElement('div')
     firstBox.innerHTML = question
     document.body.append(firstBox)
     firstBox.setAttribute("id", "question");
+}
+const secondBoxCreator = function (answer, answer1, answer2, answe3) {
+    let secondBox = document.createElement('div')
+    secondBox.innerHTML = answer
+    document.body.append(secondBox)
+    secondBox.setAttribute("id", "answer")
+    let buttonOne = document.createElement('button')
+    buttonOne.innerHTML = 'button1'
+    secondBox.appendChild(buttonOne)
 }
 
 
