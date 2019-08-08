e2de010ad6de14a3f3fb6b91d285c1808be11cd3
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-08 13:33:46 -0400

    work

--- project1.js
+++ project1.js
@@ -1,38 +1,41 @@
-//let playerOne;
-//document.getElementById('sub').addEventListener('click', function(event) {
-//let playerName = document.querySelector('#name')
+
+document.getElementById('sub').addEventListener('click', function(event) {
+document.querySelector('#play').innerText = document.querySelector('#name').value
+})
 document.getElementById('g1').addEventListener('click', () => gOne())
 const gOne = function () {
     console.log('changing this')
-    firstBoxCreator("who is the president?")
-    secondBoxCreator("a,b,c")
+    firstBoxCreator("Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a:San Francisco" + "<br>" + "<br>" + "b:Los Angeles" + "<br>" + "<br>" + "c:San Diego") 
+
+    //secondBoxCreator()
 }
 const firstBoxCreator = function (question, a, b, c) {
-
     let firstBox = document.createElement('div')
     firstBox.innerHTML = question
     document.body.append(firstBox)
     firstBox.setAttribute("id", "question");
 }
-const secondBoxCreator = function (answer, answer1, answer2, answe3) {
+const secondBoxCreator = function (answer) {
     let secondBox = document.createElement('div')
     let inputBox = document.createElement('input')
     let buttonOne = document.createElement('button')
+    buttonOne.setAttribute("id", "buttonSecond")
 
     secondBox.innerHTML = answer
     document.body.append(secondBox)
     secondBox.appendChild(inputBox)
     secondBox.setAttribute("id", "answer")
 
-    buttonOne.innerHTML = 'button1'
+    buttonOne.innerHTML = 'Submit'
     buttonOne.addEventListener('click', () => boxDisappear())
     secondBox.appendChild(buttonOne)
 }
 
 const boxDisappear = function() {
     console.log('hi')
-    document.getElementById('question').remove()
-    document.getElementById('answer').remove()
+    //document.getElementById("question").style.visibility = "hidden"; 
+    firstBoxCreator.getElementById("question").remove()
+    secondBoxCreator.getElementById("answer").remove()
 
 }
 
