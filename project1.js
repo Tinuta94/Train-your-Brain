856453e092c67f3dbc8cf2daa2520f2c59be2664
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-09 22:54:18 -0400

    ss

--- project1.js
+++ project1.js
@@ -8,6 +8,14 @@ let questions = [
         question: "What are the five oceans of the world?" + "<br>" + "<br>" + "a-Arctic,Pacific,Atlantic," + "<br>" + "Southern,Indian" + "<br>" + "<br>" + "b-Pacific,Sweet,Cold," + "<br>" + "Spicy,Indian" + "<br>" + "<br>" + "c-Hostile,Chinese," + "<br>" + "Arctic,Japanese,Blue",
         correctAnswer: "a"
     }
+    {
+        question: "Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a-San Francisco" + "<br>" + "<br>" + "b-Los Angeles" + "<br>" + "<br>" + "c-San Diego"
+        correctAnswer: "b"
+    }
+    {
+        question: "What is the highest mountain in North America?" + "<br>" + "<br>" + "a-Mount Logan" + "<b>" + "Denali" + "<br" + "Mount Saint Elias"
+        correctAnswer: "b"
+    }
 ]
 
 for(let i = 0; i < 3; i++) {
@@ -37,7 +45,7 @@ document.getElementById('g2').addEventListener('click', () => gTwo())
 const gTwo = function () {
     valueOfg2 = parseInt(document.getElementById('g2').innerText)
     console.log(valueOfg2)
-   
+    document.getElementById('g2').style.visibility="hidden"
     firstBoxCreator("Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a-San Francisco" + "<br>" + "<br>" + "b-Los Angeles" + "<br>" + "<br>" + "c-San Diego") 
     secondBoxCreator("<br>" + "Pick the correct answer" + "<br> " + "Type a,b" + " " + "or c" + "<br>", valueOfg2, 'b') 
 }
@@ -46,15 +54,15 @@ document.getElementById('g3').addEventListener('click', () => gThree())
 const gThree = function () {
     valueOfg3 = parseInt(document.getElementById('g3').innerText)
     console.log(valueOfg3)
-   
-    firstBoxCreator("Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a-San Francisco" + "<br>" + "<br>" + "b-Los Angeles" + "<br>" + "<br>" + "c-San Diego") 
-    secondBoxCreator("<br>" + "Pick the correct answer" + "<br> " + "Type a,b" + " " + "or c" + "<br>", valueOfg3, 'c') 
+    document.getElementById('g3').style.visibility="hidden"
+    firstBoxCreator("<br>" + "What is the highest mountain in North America?" + "<br>" + "<br>" + "a-Mount Logan" + "<br>" + "<br>" + "b-Denali" + "<br>" +"<br>" + "c-Mount Saint Elias") 
+    secondBoxCreator("<br>" + "Pick the correct answer" + "<br> " + "Type a,b" + " " + "or c" + "<br>", valueOfg3, 'b') 
 }
 document.getElementById('m1').addEventListener('click', () => mOne())
 const mOne = function () {
     valueOfm1 = parseInt(document.getElementById('m1').innerText)
     console.log(valueOfm1)
-   
+    document.getElementById('m1').style.visibility="hidden"
     firstBoxCreator("Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a-San Francisco" + "<br>" + "<br>" + "b-Los Angeles" + "<br>" + "<br>" + "c-San Diego") 
     secondBoxCreator("<br>" + "Pick the correct answer" + "<br> " + "Type a,b" + " " + "or c" + "<br>", valueOfm1, 'c') 
 }
@@ -62,7 +70,7 @@ document.getElementById('m2').addEventListener('click', () => mTwo())
 const mTwo = function () {
     valueOfm2 = parseInt(document.getElementById('m2').innerText)
     console.log(valueOfm2)
-   
+    document.getElementById('m2').style.visibility="hidden"
     firstBoxCreator("Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a-San Francisco" + "<br>" + "<br>" + "b-Los Angeles" + "<br>" + "<br>" + "c-San Diego") 
     secondBoxCreator("<br>" + "Pick the correct answer" + "<br> " + "Type a,b" + " " + "or c" + "<br>", valueOfm2, 'b') 
 }
@@ -70,7 +78,7 @@ document.getElementById('m3').addEventListener('click', () => mThree())
 const mThree = function () {
     valueOfm3 = parseInt(document.getElementById('m3').innerText)
     console.log(valueOfm3)
-   
+    document.getElementById('m3').style.visibility="hidden"
     firstBoxCreator("Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a-San Francisco" + "<br>" + "<br>" + "b-Los Angeles" + "<br>" + "<br>" + "c-San Diego") 
     secondBoxCreator("<br>" + "Pick the correct answer" + "<br> " + "Type a,b" + " " + "or c" + "<br>", valueOfm3, 'a') 
 }
@@ -78,7 +86,7 @@ document.getElementById('h1').addEventListener('click', () => hOne())
 const hOne = function () {
     valueOfh1 = parseInt(document.getElementById('h1').innerText)
     console.log(valueOfh1)
-   
+    document.getElementById('h1').style.visibility="hidden"
     firstBoxCreator("Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a-San Francisco" + "<br>" + "<br>" + "b-Los Angeles" + "<br>" + "<br>" + "c-San Diego") 
     secondBoxCreator("<br>" + "Pick the correct answer" + "<br> " + "Type a,b" + " " + "or c" + "<br>", valueOfh1, 'a') 
 }
@@ -86,7 +94,7 @@ document.getElementById('h2').addEventListener('click', () => hTwo())
 const hTwo = function () {
     valueOfh2 = parseInt(document.getElementById('h2').innerText)
     console.log(valueOfh2)
-   
+    document.getElementById('h2').style.visibility="hidden"
     firstBoxCreator("Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a-San Francisco" + "<br>" + "<br>" + "b-Los Angeles" + "<br>" + "<br>" + "c-San Diego") 
     secondBoxCreator("<br>" + "Pick the correct answer" + "<br> " + "Type a,b" + " " + "or c" + "<br>", valueOfh2, 'a') 
 }
@@ -94,7 +102,7 @@ document.getElementById('h3').addEventListener('click', () => hThree())
 const hThree = function () {
     valueOfh3 = parseInt(document.getElementById('h3').innerText)
     console.log(valueOfh3)
-   
+    document.getElementById('h3').style.visibility="hidden"
     firstBoxCreator("Beverly Hills is a suburb of which U.S. city??" + "<br>" + "<br>" +"a-San Francisco" + "<br>" + "<br>" + "b-Los Angeles" + "<br>" + "<br>" + "c-San Diego") 
     secondBoxCreator("<br>" + "Pick the correct answer" + "<br> " + "Type a,b" + " " + "or c" + "<br>", valueOfh3, 'a') 
 }
@@ -169,17 +177,15 @@ let showStuff = function() {
 }
 
 function hidebox() {
-   // let fBox = document.getElementById('g1')
+   //let fBox = document.getElementById('g1')
     //fBox.style.visibility="hidden"
 }
 
 document.getElementById('gameover').addEventListener('click', function(event) {
 let totalScore = document.querySelector('#currentScore').innerText = newCurrentScore
 if (totalScore >= 500) {
-    document.getElementById("txtgm").style.display = "block";
-console.log('you win')
+    document.getElementById("txtgm").style.display = "block"
 } else {
-    alert("Do you want to play again?")
-    console.log('you lost')
+  document.getElementById("egame").style.display = "block"
 }
 })
